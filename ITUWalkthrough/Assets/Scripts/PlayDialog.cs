4e7b9b1dc80c88fbd670be219f75bac1e92f5523
TRain2010 <wliangtianyu@gmail.com>
2021-04-24 05:04:55 -0500

    Dialog demo

---
+++ ITUWalkthrough/Assets/Scripts/PlayDialog.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using VIDE_Data;
+
+public class PlayDialog : MonoBehaviour
+{
+    public string playerName = "Player";
+
+
+    //Reference to our diagUI script for quick access
+    public VIDEUIManager1 diagUI;
+    //Stored current VA when inside a trigger
+    public VIDE_Assign inTrigger;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<VIDE_Assign>() != null)
+            inTrigger = other.GetComponent<VIDE_Assign>();
+    }
+
+    void OnTriggerExit()
+    {
+        inTrigger = null;
+    }
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        //Interact with NPCs when pressing E
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TryInteract();
+        }
+
+        //Hide/Show cursor
+        if (Input.GetMouseButtonDown(0))
+        {
+            Cursor.visible = !Cursor.visible;
+            if (Cursor.visible)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    void TryInteract()
+    {
+        /* Prioritize triggers */
+
+        if (inTrigger)
+        {
+            diagUI.Interact(inTrigger);
+            return;
+        }
+
+        /* If we are not in a trigger, try with raycasts */
+
+        RaycastHit rHit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out rHit, 2))
+        {
+            //Lets grab the NPC's VIDE_Assign script, if there's any
+            VIDE_Assign assigned;
+            if (rHit.collider.GetComponent<VIDE_Assign>() != null)
+                assigned = rHit.collider.GetComponent<VIDE_Assign>();
+            else return;
+
+            diagUI.Interact(assigned); //Begins interaction
+        }
+    }
+
+    
+}
